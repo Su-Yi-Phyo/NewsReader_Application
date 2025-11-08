78487cb47237d67ed485af11aa6625df439656ba
Trương Lê Hiếu An <52300005@student.tdtu.edu.vn>
2025-11-08 17:06:39 +0700

    layout transition

--- app/src/main/java/com/example/newsreaderapp/activities/MainActivity.java
+++ app/src/main/java/com/example/newsreaderapp/activities/MainActivity.java
@@ -5,6 +5,9 @@ import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.newsreaderapp.R;
@@ -20,13 +23,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         bottomNavigationView = findViewById(R.id.bottom_nav);
 
         // Mặc định mở tab "Tin tức"
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragmentContainer, new HomePageFragment())
+                .replace(R.id.frame_container, new HomePageFragment())
                 .commit();
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -47,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
             if (selected != null) {
                 getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.fragmentContainer, selected)
+                        .replace(R.id.frame_container, selected)
                         .commit();
             }
 
